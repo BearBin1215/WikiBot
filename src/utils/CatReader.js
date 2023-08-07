7012f7b36ed5370bc3891841ab606f15437489b6
BearBin1215 <20369414+BearBin1215@users.noreply.github.com>
2023-08-07 10:53:40 +0800

    feat: 提供递归查询子页面功能

--- src/utils/CatReader.js
+++ src/utils/CatReader.js
@@ -9,12 +9,14 @@ const catReader = {
     formatter: (title) => title.replace(/^(?:Category:|Cat:|分[类類]:)?(.*)$/i, "Category:$1"),
 
     /**
-     * 获取指定分类下所有页面
+     * 获取指定分类的成员
      * 
      * @param {string} title 分类名
+     * @param {string} cmtype 要获取的成员类型，默认为page
+     * @param {string|number} cmnamespace 指定的名字空间，默认获取全部
      * @returns {Array} 分类成员列表
      */
-    getMembers: async function (title) {
+    getMembers: async function (title, cmtype = "page", cmnamespace = "*") {
         let cmcontinue = "";
         const pageList = [];
         if(title.length === 0) {
@@ -29,6 +31,8 @@ const catReader = {
                     cmlimit: "max",
                     cmtitle,
                     cmcontinue,
+                    cmnamespace,
+                    cmtype,
                 }).catch((err) => {
                     throw err;
                 });
@@ -45,14 +49,24 @@ const catReader = {
         return pageList;
     },
 
-    /**
-     * 获取指定分类的子分类
-     * 
-     * @param {string} title 分类名
-     * @returns {Array} 子分类列表
-     */
-    getSubs: async function (title) {
 
+    /**
+     * 递归获取分类下所有页面
+     * @param {string} title 分类名
+     * @param {string|number} cmnamespace 要获取的名字空间，默认全部
+     * @returns 
+     */
+    traverseCat: async function (title, cmnamespace = "*") {
+        const pageList = [];
+        const members = await this.getMembers(title, "page", cmnamespace);
+        const subCats = await this.getMembers(title, "subcat");
+        console.log(`获取到${title}内页面${members.length}个，子分类${subCats.length}个。`);
+        pageList.push(...members);
+        for (const item of subCats) {
+            const subMembers = await this.traverseCat(item);
+            pageList.push(...subMembers);
+        }
+        return [...new Set(pageList)]; // 去重
     },
 };
 
