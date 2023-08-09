e0551f14b9b88b48205c564ea5ab5868e8f17a57
BearBin1215 <20369414+BearBin1215@users.noreply.github.com>
2023-08-09 21:17:36 +0800

    style

--- config/config.js
+++ config/config.js
@@ -3,10 +3,10 @@
  */
 
 const config = {
-    "API_PATH": "https://mzh.moegirl.org.cn/api.php", // api.php地址
-    "CM_API_PATH": "https://commons.moegirl.org.cn/api.php",
-    "username": "", // wiki上的用户名
-    "password": "", // wiki上的密码
+    API_PATH: "https://mzh.moegirl.org.cn/api.php", // api.php地址
+    CM_API_PATH: "https://commons.moegirl.org.cn/api.php",
+    username: "", // wiki上的用户名
+    password: "", // wiki上的密码
 };
 
 // 自动更新用
